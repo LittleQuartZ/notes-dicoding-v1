c59c94881fc61c65de86a0fcf34f10567e07648f
Syahdan <58659387+LittleQuartZ@users.noreply.github.com>
2023-01-09 19:31:52 +0700

    feat: initial commit of unocss

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "uno.css";
+import "@unocss/reset/tailwind.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
